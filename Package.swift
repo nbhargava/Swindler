744569a837f41bb6bee9acb8d045af104576dbf3
nbhargava <nikhilb@cs.stanford.edu>
2019-12-14 17:23:16 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/nbhargava/AXSwift.git", from:"0.2.4")
+        .package(url: "https://github.com/nbhargava/AXSwift.git", from: "0.2.4"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.10.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Swindler",
-            dependencies: ["AXSwift"],
+            dependencies: ["AXSwift", "PromiseKit"],
             path: "Sources"),
         .testTarget(
             name: "SwindlerTests",
